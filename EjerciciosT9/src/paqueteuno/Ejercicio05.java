734a5d991f34e42ee843593aea356235e0a88d7b
René Rolando Elizalde Solano <reroes799@gmail.com>
2020-12-18 00:10:42 -0500

    Initial commit

---
+++ EjerciciosT9/src/paqueteuno/Ejercicio05.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteuno;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejercicio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String[] estudiantes = {"Kimberly", "Hogan", 
+            "Teresa", "Luis", "Mark", "Jennifer",
+            "Alcides"};
+        String inicial;
+        boolean bandera = true;
+        while(bandera){
+            System.out.println("Ingrese una letra");
+            inicial = entrada.nextLine();
+                    
+        }
+        
+        
+    }
+
+}
